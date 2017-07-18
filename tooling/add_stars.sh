0ef0575007108bf41c6563085112c4483dd05b38
Richard Littauer <richard.littauer@gmail.com>
2017-07-18 13:45:17 -0400

    Changing env

--- tooling/add_stars.sh
+++ tooling/add_stars.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Add GitHub stars if it doesn't exist
 sed -i "" '/GitHub stars/! s/^\* \[\(.*\)\](https:\/\/github.com\/\([^ )]*\))\(.*\)/* [\1 \![GitHub stars]\(https:\/\/img.shields.io\/github\/stars\/\2.svg\)]\(https:\/\/github.com\/\2\)\3/' README.md
